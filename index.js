2595d499321a55f69049bba1c960a474e8548897
Rakhshan khan <rakshaankhan1@gmail.com>
2024-02-09 23:46:50 +0530

    Add home route adn udate-studentProfile for testing Purpose

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const { connectToMongoDB } = require('./connection')
 
-
-connectToMongoDB('mongodb://127.0.0.1:27017/School_Management_System')
-.then(()=>{ console.log('MongoDB connected')})
-
 const app = express()
 const port = 3000
+
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+connectToMongoDB('mongodb://127.0.0.1:27017/School_Management_System').then(
+  () => {
+    console.log('MongoDB connected')
+  }
+)
+
+
+app.get('/', (req, res) => {
+  return res.send('<h1>Hello</h1>')
+})
+
+app.patch('/update-studentprofile',(req, res) => {
+  console.log(req)
+})
+
+
 
 
 
